e664387b934af411be1bee583fa664ce0573a082
Iiro Jäppinen <iiro@jappinen.fi>
2019-02-06 07:19:44 +0200

    refactor: use html from common-tags to format output

--- src/renderer.js
+++ src/renderer.js
@@ -1,7 +1,7 @@
 import "@babel/polyfill";
 
 import { ChunkExtractor, ChunkExtractorManager } from "@loadable/server";
-import { minify } from "html-minifier";
+import { html } from "common-tags";
 import React from "react";
 import ReactDOMServer from "react-dom/server";
 import { HelmetProvider } from "react-helmet-async";
@@ -27,36 +27,28 @@ export default function renderer({ compilationAssets, path, stats }) {
 
   const { helmet } = helmetContext;
 
-  return minify(
-    `
-      <!DOCTYPE html>
-      <html lang="en" ${helmet.htmlAttributes.toString()}>
-        <head>
-          <meta name="version" content="${stats.hash}" />
-          <meta charset="utf-8" />
-          ${helmet.title.toString()}
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
-          ${helmet.meta.toString()}
-          <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-          <link rel="apple-touch-icon" sizes="192x192" href="/icon-192.png" />
-          <link rel="apple-touch-icon" sizes="512x512" href="/icon-512.png" />
-          ${helmet.link.toString()}
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          ${extractor.getLinkTags()}
-        </head>
-        <body ${helmet.bodyAttributes.toString()}>
-          <div id="root">${app}</div>
-          ${extractor.getScriptTags()}
-        </body>
-      </html>
-    `,
-    {
-      collapseWhitespace: true,
-      preserveLineBreaks: true
-    }
-  );
+  /* eslint-disable prettier/prettier */
+  return html`
+    <!DOCTYPE html>
+    <html lang="en" ${helmet.htmlAttributes.toString()}>
+      <head>
+        <meta name="version" content="${stats.hash}" />
+        <meta charset="utf-8" />
+        ${helmet.title.toString()}
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        ${helmet.meta.toString()}
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="apple-touch-icon" sizes="192x192" href="/icon-192.png" />
+        <link rel="apple-touch-icon" sizes="512x512" href="/icon-512.png" />
+        ${helmet.link.toString()}
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        ${extractor.getLinkTags()}
+      </head>
+      <body ${helmet.bodyAttributes.toString()}>
+        <div id="root">${app}</div>
+        ${extractor.getScriptTags()}
+      </body>
+    </html>
+  `.replace(/^\s*$(?:\r\n?|\n)/gm, "");
 }
